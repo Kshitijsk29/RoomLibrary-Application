e50f5f4c00cee635ec085f44e07ed3972582f90e
kshitij <albatros.29kr@gmail.com>
2024-06-09 02:48:37 +0530

    second commit

--- app/src/main/java/com/nextin/roomlibraryapplication/AppDatabase.kt
+++ app/src/main/java/com/nextin/roomlibraryapplication/AppDatabase.kt
@@ -1,11 +1,35 @@
 package com.nextin.roomlibraryapplication
 
+import android.content.Context
 import androidx.room.Database
 import androidx.room.Room
 import androidx.room.RoomDatabase
+import androidx.room.TypeConverter
+import androidx.room.TypeConverters
 
 
 @Database(entities =[UserEntity::class], version = 1)
+@TypeConverters(Convetors::class)
 abstract class AppDatabase : RoomDatabase(){
     abstract fun  userDao():UserDao
+
+    companion object{
+        @Volatile
+        private var INSTANCE : AppDatabase? = null
+
+        fun getDatabase(context: Context):AppDatabase{
+
+            if(INSTANCE == null){
+                synchronized(this){
+                    INSTANCE = Room.databaseBuilder(
+                        context.applicationContext,
+                        AppDatabase::class.java,
+                        "contactDB"
+                    ).build()
+                }
+            }
+
+            return INSTANCE!!
+        }
+    }
 }
